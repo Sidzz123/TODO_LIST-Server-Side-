c9ee86a59099686dd0c88340aae9e8f4c3215c39
Siddharth Bhandari <sidzzatwork22@gmail.com>
2025-08-11 00:52:59 +0530

    New Client url

--- Index.js
+++ Index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 // Allow requests from frontend origin
 app.use(cors({
-  origin: 'https://todo-list-client-side.vercel.app/', // replace with your React app's URL
+  origin: '*', // replace with your React app's URL
   credentials: true               // allow cookies if needed
 }));
   
